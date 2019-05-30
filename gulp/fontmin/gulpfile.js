425c94d65ff94f682665555ea8725c991d0f503b
ntnyq <ntnyq13@gmail.com>
2019-05-30 15:47:48 +0800

    feat: 添加字体压缩脚本

---
+++ gulp/fontmin/gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const fontmin = require('gulp-fontmin')
+const { text } = require('./config')
+
+gulp.task('default', function () {
+  return (
+    gulp
+      .src('fonts/**/*.ttf')
+      .pipe(fontmin({ text }))
+      .pipe(gulp.dest('dist/fonts'))
+  )
+})
---
+++ rxjs/rxjs-learn/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/main.js',
+
+  output: {
+    path: `${__dirname}/public`,
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: `${__dirname}/src`
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    open: true
+  }
+}
